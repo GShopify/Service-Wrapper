98c04b772482bfc98d74bb7757b816a2a088eacc
Alexey Kim <alexey.kim@gshopper.com>
2022-11-04 17:07:05 +0900

    DateTime
    - refactoring

--- scalar/DateTime.go
+++ scalar/DateTime.go
@@ -2,23 +2,44 @@ package scalar
 
 import (
 	"encoding/json"
+	"io"
+	"strconv"
 	"time"
 )
 
-type DateTime struct {
-	time.Time
-}
+type DateTime int64
 
 func NewDateTimeIn(seconds int) DateTime {
-	return DateTime{
-		time.Now().Add(time.Duration(seconds) * time.Second),
-	}
-}
-
-func (d DateTime) MarshalJSON() ([]byte, error) {
-	return json.Marshal(d.Time)
+	return DateTime(
+		time.Now().Add(time.Duration(seconds) * time.Second).UnixMilli())
 }
 
 func (d DateTime) String() string {
-	return d.Time.String()
+	return time.UnixMilli(int64(d)).String()
+}
+
+func (d DateTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.UnixMilli(int64(d)))
+}
+
+func (d DateTime) MarshalGQL(w io.Writer) {
+	w.Write([]byte(strconv.Quote(d.String())))
+}
+
+func (d *DateTime) UnmarshalGQL(v interface{}) error {
+	switch v := v.(type) {
+	case float64:
+		*d = DateTime(v)
+	case int:
+		*d = DateTime(v)
+	case string:
+		t, err := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", v)
+		if err != nil {
+			return err
+		}
+
+		*d = DateTime(t.UnixMilli())
+	}
+
+	return nil
 }
